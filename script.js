62e48754110a271a3eb51747deae698f6ea6ba18
Pedro Amaral <amaralph1995@gmail.com>
2020-11-11 18:18:51 -0300

    Requisito 6 - corrige cores das bordas

--- script.js
+++ script.js
@@ -8,21 +8,19 @@ window.onload = function() {
     const memeInsert = document.querySelector('#meme-insert');
     const memeImage = document.querySelector('#meme-image');
     memeInsert.addEventListener('click', function() {
-        console.log(memeInsert.value);
         memeImage.src = memeInsert.value;
     })
 
     const imageContainer = document.querySelector('#meme-image-container');
     addEventListener('click', function(event) {
         let botao = event.target;
-        console.log(botao)
         if (botao.classList.contains('change-border')) {
             if (botao.id == 'fire') {
-                imageContainer.style.border = '3px dashed rgba(228, 49, 49, 0.822)';
+                imageContainer.style.border = '3px dashed red';
             } else if (botao.id == 'water') {
-                imageContainer.style.border = '5px double rgba(49, 127, 228, 0.822)';
+                imageContainer.style.border = '5px double blue';
             } else if (botao.id == 'earth') {
-                imageContainer.style.border = '6px groove rgba(136, 228, 49, 0.822)';
+                imageContainer.style.border = '6px groove green';
             }
         }
     })
