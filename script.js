fe523aad17b430ffdfed1f01d69b3a4c94ceec1b
Daniel <danieldsrd@gmail.com>
2020-11-11 18:01:28 -0300

    botao estilizar

--- script.js
+++ script.js
@@ -12,12 +12,15 @@ document.querySelector('#text-input').addEventListener('keyup', function (event)
 
 document.querySelector('#fire').addEventListener('click', function(){
     document.querySelector('#meme-image-container').style.border = '3px dashed red'
+    document.querySelector('#meme-image-container').style.background = 'red'
 });
 
 document.querySelector('#water').addEventListener('click', function(){
     document.querySelector('#meme-image-container').style.border = '5px double blue'
+    document.querySelector('#meme-image-container').style.background = 'blue'    
 });
 
 document.querySelector('#earth').addEventListener('click', function(){
     document.querySelector('#meme-image-container').style.border = '6px groove green'
+    document.querySelector('#meme-image-container').style.background = 'green'
 });
