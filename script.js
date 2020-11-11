86555aaa3c29867d1c3f849fa8d3f141f0e686bf
Daniel <danieldsrd@gmail.com>
2020-11-11 18:17:48 -0300

    botao estilizar

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ document.querySelector('#text-input').addEventListener('keyup', function (event)
 //BOTAO ESTILIZAR
 
 document.querySelector('#fire').addEventListener('click', function(){
-    document.querySelector('#meme-image-container').style.border = '3px dashed red'
-    document.querySelector('#meme-image-container').style.background = 'red'
+    document.querySelector('#meme-image-container').style.border = '3px dashed red';
+    document.querySelector('#meme-image-container').style.background = 'rgb(255, 0, 0)';
 });
 
 document.querySelector('#water').addEventListener('click', function(){
     document.querySelector('#meme-image-container').style.border = '5px double blue'
-    document.querySelector('#meme-image-container').style.background = 'blue'    
+    document.querySelector('#meme-image-container').style.background = 'rgb(0, 0, 255)';
 });
 
 document.querySelector('#earth').addEventListener('click', function(){
-    document.querySelector('#meme-image-container').style.border = '6px groove green'
-    document.querySelector('#meme-image-container').style.background = 'green'
+    document.querySelector('#meme-image-container').style.border = '6px groove green';
+    document.querySelector('#meme-image-container').style.background = 'rgb(0, 128, 0)';
 });
