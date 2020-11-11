3ffb429fa236c9505402c5321134c4214ff848ad
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-11-11 16:59:12 -0300

    site funcionando

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-//  Adicionar texto do input ao meme
 const inputMeme = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
-const memeContainer = document.querySelector('#meme-image-container')
+const memeContainer = document.querySelector('#meme-image-container');
+const memeImage = document.querySelector('#meme-image');
+const memeInsert = document.querySelector('#meme-insert');
 
+//  Adicionar texto do input ao meme
 inputMeme.addEventListener('input', function () {
   memeText.innerText = inputMeme.value;
 });
 
-memeText.style.widht = memeContainer.style.widht
+//  Adicionar Imagem do input ao meme
+memeInsert.addEventListener('input', function () {
+  const url = URL.createObjectURL(memeInsert.files[0])
+    memeImage.src = url
+})
