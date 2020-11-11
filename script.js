a3e782cdfade6c99140be6de99cbe672660c9e34
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-11-11 16:21:59 -0300

    adicionando requisito 2

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function enviaTextoMeme(evento) {
-  let textoMeme = document.querySelector("#meme-text");
+  const textoMeme = document.querySelector("#meme-text");
   textoMeme.innerText = evento.target.value;
 }
 
+function enviaImageMeme(evento) {
+    const localImage = document.querySelector('#meme-image');
+    console.log(evento.target.files)
+    localImage.src = URL.createObjectURL(evento.target.files[0]);
+}
+
 window.onload = function () {
-  let inputText = document.querySelector("#text-input");
+  const inputText = document.querySelector("#text-input");
+  const inputImage = document.querySelector('#meme-insert');
+
+  inputImage.addEventListener('change', enviaImageMeme);
   inputText.addEventListener('input', enviaTextoMeme);
 };
