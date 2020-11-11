eef51903cff31ae3af5ef1060b603a63009931f9
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-11-11 17:45:15 -0300

    adicionando requisito 6

--- script.js
+++ script.js
@@ -4,15 +4,41 @@ function enviaTextoMeme(evento) {
 }
 
 function enviaImageMeme(evento) {
-    const localImage = document.querySelector('#meme-image');
-    console.log(evento.target.files)
-    localImage.src = URL.createObjectURL(evento.target.files[0]);
+  const localImage = document.querySelector("#meme-image");
+  console.log(evento.target.files);
+  localImage.src = URL.createObjectURL(evento.target.files[0]);
 }
+
+function colorirBordaFire() {
+    const container = document.querySelector('#meme-image-container')
+    container.style.border = '3px dashed red'
+}
+
+function colorirBordaWater() {
+    const container = document.querySelector('#meme-image-container')
+    container.style.border = '5px double blue'
+}
+
+function colorirBordaEarth() {
+    const container = document.querySelector('#meme-image-container')
+    container.style.border = '6px groove green'
+}
+
+
+
+
 
 window.onload = function () {
   const inputText = document.querySelector("#text-input");
-  const inputImage = document.querySelector('#meme-insert');
+  const inputImage = document.querySelector("#meme-insert");
+  const btnFire = document.querySelector("#fire");
+  const btnWater = document.querySelector("#water");
+  const btnEarth = document.querySelector("#earth");
 
-  inputImage.addEventListener('change', enviaImageMeme);
-  inputText.addEventListener('input', enviaTextoMeme);
+  inputImage.addEventListener("change", enviaImageMeme);
+  inputText.addEventListener("input", enviaTextoMeme);
+
+  btnFire.addEventListener('click', colorirBordaFire)
+  btnWater.addEventListener('click', colorirBordaWater)
+  btnEarth.addEventListener('click', colorirBordaEarth)
 };
