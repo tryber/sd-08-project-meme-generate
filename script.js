d7abd0d2d48ed594ac4760c196f4f95de5561272
Pedro Amaral <amaralph1995@gmail.com>
2020-11-11 17:23:15 -0300

    Requisito 5 - limita caracteres do input

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ window.onload = function() {
     textInput.addEventListener('input', function() {
         memeText.innerHTML = textInput.value;
     })
+
+    const memeInsert = document.querySelector('#meme-insert');
+    const memeImage = document.querySelector('#meme-image');
+    memeInsert.addEventListener('click', function() {
+        console.log(memeInsert.value);
+        memeImage.src = memeInsert.value;
+    })
 }
