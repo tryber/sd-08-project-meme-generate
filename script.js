bafdfee4904d11f8ed5edaaf1f65db4807edd54e
MEianelli <filipelouzano@gmail.com>
2020-11-11 14:52:25 -0300

    Requisitos obrigatorios feitos, primeiro teste

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+  document
+    .querySelector("#text-input")
+    .addEventListener("change", atualizarTexto);
+  document
+    .querySelector("#text-input")
+    .addEventListener("keydown", atualizarTexto);
+};
+
+function atualizarTexto() {
+  let memeTexto = document.querySelector("#meme-text");
+  let textArea = document.querySelector("#text-input");
+  memeTexto.innerHTML = textArea.value;
+}
+
+function readURL(input) {
+  var imgDiv = document.querySelector("#meme-image-img");
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      imgDiv.src = e.target.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
