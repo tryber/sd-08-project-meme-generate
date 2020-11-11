6046a1e49f633d9a42b2e82ac74bfc66816b43d9
MEianelli <filipelouzano@gmail.com>
2020-11-11 15:55:35 -0300

    Requisitos bonus feitos, primeiro teste

--- script.js
+++ script.js
@@ -5,7 +5,22 @@ window.onload = function () {
   document
     .querySelector("#text-input")
     .addEventListener("keydown", atualizarTexto);
+
+  document.querySelector("#fire").addEventListener("click", aplicarFogo);
+  document.querySelector("#water").addEventListener("click", aplicarAgua);
+  document.querySelector("#earth").addEventListener("click", aplicarTerra);
+
+  document.querySelectorAll(".preLoadedMemes img").forEach((e) => {
+    e.addEventListener("click", (evt) => {
+      adicionarImagem(evt.target.src);
+    });
+  });
 };
+
+function adicionarImagem(source) {
+  let imgDiv = document.querySelector("#meme-image-img");
+  imgDiv.src = source;
+}
 
 function atualizarTexto() {
   let memeTexto = document.querySelector("#meme-text");
@@ -20,10 +35,26 @@ function readURL(input) {
     var reader = new FileReader();
 
     reader.onload = function (e) {
+      imgDiv.src = "#";
       imgDiv.src = e.target.result;
     };
 
     reader.readAsDataURL(input.files[0]);
     spanBtn.innerHTML = input.files[0].name;
   }
+}
+
+function aplicarFogo() {
+  let memeContainer = document.querySelector("#meme-image-container");
+  memeContainer.style.border = "3px dashed red";
+}
+
+function aplicarAgua() {
+  let memeContainer = document.querySelector("#meme-image-container");
+  memeContainer.style.border = "5px double blue";
+}
+
+function aplicarTerra() {
+  let memeContainer = document.querySelector("#meme-image-container");
+  memeContainer.style.border = "6px groove green";
 }
