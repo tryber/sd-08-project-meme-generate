09bd8a5b98e0b5831ddbfa951db12d0b0ce56648
Luiz Reina <luizreeina@gmail.com>
2020-11-12 01:56:47 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const input = document.getElementById('text-input');
 const containerImagem = document.getElementById('meme-image');
+const containerPrincipal = document.getElementById('meme-image-container');
 const escolherImagem = document.getElementById('meme-insert');
 const textoMeme = document.getElementById('meme-text');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
 
 function alteraTextoMeme() {
   textoMeme.innerText = input.value;
 }
 
 function mostraImagemEscolhida() {
-  const curlFiles = escolherImagem.files;
-  const imagemEscolhida = URL.createObjectURL(curlFiles[0]);
+  const imagemEscolhida = URL.createObjectURL(escolherImagem.files[0]);
   containerImagem.style.backgroundImage = `url('${imagemEscolhida}')`;
+}
+
+function trocaBordaContainer(evento) {
+  switch(evento.target.id) {
+    case 'fire':
+    containerPrincipal.style.border = 'dashed 3px red';
+    break;
+
+    case 'water':
+    containerPrincipal.style.border = 'double 5px blue';
+    break;
+
+    case 'earth':
+    containerPrincipal.style.border = 'groove 6px green';
+    break;
+  }
 }
 
 input.addEventListener('keyup', alteraTextoMeme);
 escolherImagem.addEventListener('change', mostraImagemEscolhida);
+btnFire.addEventListener('click', trocaBordaContainer)
+btnWater.addEventListener('click', trocaBordaContainer)
+btnEarth.addEventListener('click', trocaBordaContainer)
