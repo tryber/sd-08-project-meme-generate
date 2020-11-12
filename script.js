e7c365dbc50d5e391ecf74a3f08baed4b87019c5
Victor Leone de Oliveira <vileoleone@gmail.com>
2020-11-12 13:32:14 -0300

    Requisitos 1 ao 5

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-let textInsideMeme =document.getElementById('meme-text');
-let inputByUserForMeme =document.getElementById('text-input')
+let inputByUserForMeme = document.getElementById('text-input');
+let buttonforImageSubmit = document.getElementById('image-submit');
+
 //
 //Adicionando evento para inserir texto na caixa de memes
 inputByUserForMeme.addEventListener('keyup',function () {
+    let textInsideMeme = document.getElementById('meme-text');
     textInsideMeme.innerText = inputByUserForMeme.value;
+})
+//
+//Adicionando evento para o botão que submeterá a imagem
+buttonforImageSubmit.addEventListener('click', function () {
+  let memeImageInsideContainer = document.getElementById('meme-image');
+  let imageTheUserChose = document.getElementById('meme-insert').files[0];
+  let imageSelectedURL = URL.createObjectURL(imageTheUserChose);
+  memeImageInsideContainer.src =imageSelectedURL;
+  memeImageInsideContainer.onload = function () {
+    URL.revokeObjectURL(imageInput.src);
+  }
 })
