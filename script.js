62433f87f34cf79ccc850cc1a87d834a353154cc
Paulo Simões <paulohbsimoes@gmail.com>
2020-11-11 22:54:19 -0300

    Requisito 2 concluido

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const memeImg = document.getElementById('meme-image');
+const imgInput = document.getElementById('meme-insert');
+const reader = new FileReader();
 
 function changeMemeText() {
   const text = textInput.value;
   memeText.textContent = text;
 }
 
-textInput.addEventListener("input", changeMemeText)
+textInput.addEventListener("input", changeMemeText);
+
+function updateImage() {
+  memeImg.src = reader.result;
+}
+
+reader.addEventListener('load', updateImage)
+
+imgInput.addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  if (file) reader.readAsDataURL(file);
+});
