ff06439d34f0bb55e6ef936ce9266f8dc5539613
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-12 17:37:36 -0300

    Req2: Corrge forma de adicionar imagem

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ const body = document.querySelector('body');
 
 window.onload = starterFunctions;
 
-function starterFunctions(){
+function starterFunctions() {
   addListenerToTextToMeme();
 }
 
-function addListenerToTextToMeme(){
+function addListenerToTextToMeme() {
   let textInput = document.querySelector('#text-input');
   let memeText = document.querySelector('#meme-text')
-  textInput.addEventListener('keyup', function(event){
+  textInput.addEventListener('keyup', function (event) {
     memeText.innerHTML = event.target.value;
   })
 }
 
 function handleFiles(event) {
-    let img = document.querySelector('#meme-insert');
-    img.setAttribute('src','');
-    img.src = URL.createObjectURL(event.target.files[0]);
-    img.onload = function() {
-        URL.revokeObjectURL(img.src) // free memory
-      }
-    /* now you can work with the file list */
+  let img = document.querySelector('#meme-image');
+  img.setAttribute('src', '');
+  img.src = URL.createObjectURL(event.target.files[0]);
+  img.onload = function () {
+    URL.revokeObjectURL(img.src) // free memory
   }
+  /* now you can work with the file list */
+}
