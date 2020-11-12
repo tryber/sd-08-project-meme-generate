f1b886947b0b470f9cdddca18abe07120b26a2b9
emassaki <massakihirayama@gmail.com>
2020-11-12 10:34:56 -0300

    Adicionado requisito 2 - adiciona a imagem ao meme container

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ textInput.addEventListener('keyup', (event) => {
   upperText.innerHTML = textInput.value;
   console.log(event.key)
 }) 
+
+// Adiciona a imagem ao container
+const imageInput = document.getElementById('meme-insert');
+imageInput.addEventListener('change', (event) => {
+  const memeImage = document.getElementById('meme-image');
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = function () {
+    URL.revokeObjectURL(imageInput.src);
+  };
+});
