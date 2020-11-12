4a6c79dfa0e88197fdda26921fa6a7cc29d748d0
Victor Leone de Oliveira <vileoleone@gmail.com>
2020-11-12 14:01:30 -0300

    Codeclimate compliance_1

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-let inputByUserForMeme = document.getElementById('text-input');
-let buttonforImageSubmit = document.getElementById('image-submit');
-
+const inputByUserForMeme = document.getElementById('text-input');
+const buttonforImageSubmit = document.getElementById('image-submit');
 //
 //Adicionando evento para inserir texto na caixa de memes
-inputByUserForMeme.addEventListener('keyup',function () {
-    let textInsideMeme = document.getElementById('meme-text');
-    textInsideMeme.innerText = inputByUserForMeme.value;
+inputByUserForMeme.addEventListener('keyup', function () {
+  const textInsideMeme = document.getElementById('meme-text');
+  textInsideMeme.innerText = inputByUserForMeme.value;
 })
 //
 //Adicionando evento para o botão que submeterá a imagem
 buttonforImageSubmit.addEventListener('click', function () {
-  let memeImageInsideContainer = document.getElementById('meme-image');
+  const memeImageInsideContainer = document.getElementById('meme-image');
   let imageTheUserChose = document.getElementById('meme-insert').files[0];
   let imageSelectedURL = URL.createObjectURL(imageTheUserChose);
-  memeImageInsideContainer.src =imageSelectedURL;
+  memeImageInsideContainer.src = imageSelectedURL;
   memeImageInsideContainer.onload = function () {
     URL.revokeObjectURL(imageInput.src);
   }
