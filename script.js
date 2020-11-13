13177657908569e35b30afcfbcb9ae1dad4c5b8b
Ediberto Oliveira <edibertooliveira@aol.com>
2020-11-13 08:18:40 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

---
+++ script.js
@@ -0,0 +1,6 @@
+const localeTextInput = document.getElementById('');
+const localeMemeInsert = document.getElementById('');
+const localememeImageContainer = document.getElementById('');
+const localeMemeText = document.getElementById('');
+const localememeImage = document.getElementById('');
+const localeButtonFileImage = document.getElementById('');
