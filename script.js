64e58cbb4f465672c300367263d588a1f2e248d0
Ediberto Oliveira <edibertooliveira@aol.com>
2020-11-13 11:28:35 -0300

    feat: question 1, 2, 3, 4, 5

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-const localeTextInput = document.getElementById('');
-const localeMemeInsert = document.getElementById('');
-const localememeImageContainer = document.getElementById('');
-const localeMemeText = document.getElementById('');
-const localememeImage = document.getElementById('');
-const localeButtonFileImage = document.getElementById('');
+const localeTextInput = document.getElementById('text-input');
+const localeMemeInsert = document.getElementById('meme-insert');
+const localeMemeText = document.getElementById('meme-text');
+const localeMemeImage = document.getElementById('meme-image');
+const localeButtonFileImage = document.getElementById('file-image');
+
+function uploadImageMeme() {
+  localeMemeInsert.click();
+}
+function previewFile(change) {
+  const file = change.target.files.item(0);
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    localeMemeImage.src = reader.result;
+  };
+  reader.readAsDataURL(file);
+  localeMemeImage.style.display = 'block';
+}
+function insertTextMeme(get) {
+  const newTextInsert = get.target.value;
+  localeMemeText.innerText = newTextInsert;
+}
+
+localeTextInput.addEventListener('keyup', insertTextMeme);
+localeButtonFileImage.addEventListener('click', uploadImageMeme);
+localeMemeInsert.addEventListener('change', previewFile);
