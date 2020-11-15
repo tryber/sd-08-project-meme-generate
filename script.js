77ca1b2aaf0d35c23142dd7d5191a1690cc65ae0
Ediberto Oliveira <edibertooliveira@aol.com>
2020-11-15 13:44:31 -0300

    feat: question 5, 6

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const localeTextInput = document.getElementById('text-input');
+const localeButtonStyleEarth = document.getElementById('earth');
+const localeButtonStyleFire = document.getElementById('fire');
+const localeButtonFileImage = document.getElementById('file-image');
+const localeButtonStyleWater = document.getElementById('water');
+const localeMemeImage = document.getElementById('meme-image');
 const localeMemeInsert = document.getElementById('meme-insert');
 const localeMemeText = document.getElementById('meme-text');
-const localeMemeImage = document.getElementById('meme-image');
-const localeButtonFileImage = document.getElementById('file-image');
+const localeTextInput = document.getElementById('text-input');
+const localeMemeImageContainer = document.getElementById('meme-image-container');
+const localeMemeImageOption = document.querySelectorAll('#meme-image-option img');
 
 function uploadImageMeme() {
   localeMemeInsert.click();
 }
 function previewFile(change) {
+  console.log(change);
   const file = change.target.files.item(0);
   const reader = new FileReader();
   reader.onloadend = () => {
@@ -20,7 +26,40 @@ function insertTextMeme(get) {
   const newTextInsert = get.target.value;
   localeMemeText.innerText = newTextInsert;
 }
+function styleBorderMeme(pixel, type, color) {
+  console.log(`${pixel}px ${type} ${color}`);
+  localeMemeImageContainer.style.border = `${pixel}px ${type} ${color}`;
+}
+function toggleImageMeme (src){
+  localeMemeImage.style.display = 'block';
+  const fileImage = src.target;
+  switch(fileImage.id) {
+    case 'meme-1':
+      localeMemeImage.src = fileImage.src;
+    case 'meme-2':
+      localeMemeImage.src = fileImage.src;
+    case 'meme-3':
+      localeMemeImage.src = fileImage.src;
+    case 'meme-4':
+      localeMemeImage.src = fileImage.src;
+    default:
+      localeMemeImage.src = fileImage.src;
+  }
+}
 
 localeTextInput.addEventListener('keyup', insertTextMeme);
-localeButtonFileImage.addEventListener('click', uploadImageMeme);
 localeMemeInsert.addEventListener('change', previewFile);
+localeMemeInsert.addEventListener('change', previewFile);
+localeButtonStyleEarth.addEventListener('click', ()=>{
+  styleBorderMeme(6, 'groove', 'green')
+});
+localeButtonStyleFire.addEventListener('click', ()=>{
+  styleBorderMeme(3,'dashed', 'red')
+});
+localeButtonFileImage.addEventListener('click', uploadImageMeme);
+localeButtonStyleWater.addEventListener('click', ()=>{
+  styleBorderMeme(5,'double', 'blue')
+});
+localeMemeImageOption.forEach((element)=>{
+  element.addEventListener('click', toggleImageMeme)
+});
