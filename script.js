1f621a289b2789aeedfe698d5b7ca6bcbe49b864
Mateus Coury <mateuscoury@gmail.com>
2020-11-16 14:00:29 -0300

    Upload de imagens no container

--- script.js
+++ script.js
@@ -5,3 +5,10 @@
 }
 criatextbox()
 */
+var loadFile = function(event) {
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+  };
