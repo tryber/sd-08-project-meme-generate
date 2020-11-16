9524a4b0a2fad092e12682838228260f598e8717
josebejohann <josebernardojohann@gmail.com>
2020-11-16 10:01:33 -0300

    Fixes codeclimate errors

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // DEFINES SELECTORS
-const textInput = document.querySelector("#text-input");
-const textMeme = document.querySelector("#meme-text");
-const container = document.querySelector(".container");
-const imageInput = document.querySelector("#meme-insert");
-const image = document.querySelector("#meme-image");
+const textInput = document.querySelector('#text-input');
+const textMeme = document.querySelector('#meme-text');
+const container = document.querySelector('.container');
+const imageInput = document.querySelector('#meme-insert');
+const image = document.querySelector('#meme-image');
 
-textInput.addEventListener("keyup", () => {
+textInput.addEventListener('keyup', () => {
   textMeme.innerHTML = textInput.value;
 });
 
-imageInput.addEventListener("change", (event) => {
+imageInput.addEventListener('change', (event) => {
   image.src = URL.createObjectURL(event.target.files[0]);
 });
 
-const loadImage = (event) => {
-  if (event.target.src) {
-    image.src = event.target.src;
-  }
-};
+// const loadImage = (event) => {
+//   if (event.target.src) {
+//     image.src = event.target.src;
+//   }
+// };
