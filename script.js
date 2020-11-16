e9d8fe431489f214d7fbfd386f0090efc12fc9c8
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-16 13:08:44 -0300

    C06 Requesito bonus 6 primeira tentativa

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let memeImageContainer = document.getElementById('meme-image-container');
 let memeText = document.getElementById('meme-text');
 let memeImage = document.getElementById('meme-image');
 let memeInsert = document.getElementById('meme-insert');
+let buttonFire = document.getElementById('fire');
+let buttonWater = document.getElementById('water');
+let buttonEarth = document.getElementById('earth');
 
 function atualizaTexto(text) {
     textInput.textContent = text.target.value;
@@ -14,3 +17,15 @@ textInput.addEventListener('input', atualizaTexto);
 function carregaImagem(insert) {
 	memeImage.src = URL.createObjectURL(insert.target.files[0]);
 }
+
+buttonFire.addEventListener('click', function() {
+	memeImageContainer.style.border = "dashed 3px red";
+})
+
+buttonWater.addEventListener('click', function() {
+	memeImageContainer.style.border = "double 5px blue";
+})
+
+buttonEarth.addEventListener('click', function() {
+	memeImageContainer.style.border = "groove 6px green";
+})
