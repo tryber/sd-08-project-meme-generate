5a0c5c4e388c409f78b7bc05b4612cc32669642b
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-11-16 21:09:27 -0300

    Teste2

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let meme_text_input = document.querySelector('#meme-text');
 let meme_image_img = document.querySelector('#meme-image');
 let meme_insert_input = document.querySelector("#meme-insert");
 
-text_input.addEventListener('keydown', toKeyPressed);
+text_input.addEventListener('keyup', toKeyPressed);
 meme_insert_input.addEventListener("change", function (event) {
     meme_image_img.src = URL.createObjectURL(event.target.files[0]);
 });
 
 function toKeyPressed() {
-    meme_text_input.innerText = text_input.value;
+    meme_text_input.innerHTML = text_input.value;
 }
