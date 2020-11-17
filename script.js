2cdf76080a7c27b88b26ae5c401238145de0314c
antonioarieiro <sfgfffs@gmail.com>
2020-11-17 20:21:00 -0300

    whats this code

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-window.onload = function(){
-    const inputImage = document.querySelector('#meme-insert');
-
-
-    inputImage.addEventListener('change', loadfle);
+function preview(event) {
+  imagem.src=URL.createObjectURL(event.target.files[0]);
 }
 
+function text(){
+  textInput = document.querySelector('#text-input')
+  textMeme = document.querySelector('#meme-text')
+  textInput.addEventListener('keyup', ()=>{
+    textMeme.innerText = textInput.value;
+  })
+}
+text()
+window.onload = function() {
+  const xablau = document.querySelector('#uploadImage')
+  xablau.addEventListener('change' , preview);
+}
 
-
-function loadfle(event) {
-    const localImage = document.querySelector('#meme-image');
-    localImage.src = URL.createObjectURL(evento.target.files[0]);
-  }
-
-
-  function addValue(v,c,m){
-    var v=['v',c,m]
-    prinf("/n INSIRA O NOME")
-    scanf("%d"),v[0]
-    prinf("/n INSIRA O MATRICULA"
-    scanf("%d"),v[1]
-    prinf("/n INSIRA O CPF")
-    scanf("%d"),v[2]
-    return v;
-  }
-  addValue('jose',12021093120,12121)
