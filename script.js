f9900b65a4f18d616e8dffd8893147712ddc0b92
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-11-17 20:41:46 -0300

    Adicionado o requisito 06

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ let text_input = document.querySelector('#text-input');
 let meme_text_input = document.querySelector('#meme-text');
 let meme_image_img = document.querySelector('#meme-image');
 let meme_insert_input = document.querySelector("#meme-insert");
+let meme_image_container = document.querySelector("#meme-image-container");
+let fire_btn = document.querySelector('#fire');
+let water_btn = document.querySelector('#water');
+let earth_btn = document.querySelector('#earth');
+
+fire_btn.addEventListener('click', changeBorder);
+water_btn.addEventListener('click', changeBorder);
+earth_btn.addEventListener('click', changeBorder);
 
 text_input.addEventListener('keyup', toKeyPressed);
 meme_insert_input.addEventListener("change", function (event) {
@@ -10,4 +18,18 @@ meme_insert_input.addEventListener("change", function (event) {
 
 function toKeyPressed() {
     meme_text_input.innerHTML = text_input.value;
+}
+
+function changeBorder(event) {
+    if (event.target.innerText == "Fire") {
+        meme_image_container.style.border = '3px dashed red';
+    }
+
+    if (event.target.innerText == "Water") {
+        meme_image_container.style.border = '5px double blue';
+    }
+
+    if (event.target.innerText == "Earth") {
+        meme_image_container.style.border = '6px groove blue';
+    }
 }
