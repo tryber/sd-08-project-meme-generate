2285698a8efa59cfada708aa985f26e1895a90e2
antonioarieiro <sfgfffs@gmail.com>
2020-11-17 14:47:14 -0300

    try again

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onload = function(){
-   const a = document.getElementById('insert');
-   a.addEventListener('change', loadFile)
+    const inputImage = document.querySelector('#meme-insert');
 
+
+    inputImage.addEventListener('change', loadfle);
 }
 
 
@@ -10,3 +11,16 @@ function loadfle(event) {
     const localImage = document.querySelector('#meme-image');
     localImage.src = URL.createObjectURL(evento.target.files[0]);
   }
+
+
+  function addValue(v,c,m){
+    var v=['v',c,m]
+    prinf("/n INSIRA O NOME")
+    scanf("%d"),v[0]
+    prinf("/n INSIRA O MATRICULA"
+    scanf("%d"),v[1]
+    prinf("/n INSIRA O CPF")
+    scanf("%d"),v[2]
+    return v;
+  }
+  addValue('jose',12021093120,12121)
