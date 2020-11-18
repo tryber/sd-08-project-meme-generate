260b1bc8f700695c9918d5302737757db8e273bc
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-17 21:22:29 -0300

    Req6 e req7: Botões de borda e meme template

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const body = document.querySelector('body');
-
 window.onload = starterFunctions;
 
 function starterFunctions() {
   addListenerToTextToMeme();
+  addListenerToBorderButtons();
+  addListenerToMemeTemplate();
 }
 
 function addListenerToTextToMeme() {
@@ -22,4 +22,35 @@ function handleFiles(event) {
     URL.revokeObjectURL(img.src) // free memory
   }
   /* now you can work with the file list */
+}
+
+function addListenerToBorderButtons() {
+  let buttonsId = ['#fire', '#water', '#earth'];
+
+  for(let index = 0; index < buttonsId.length; index += 1){
+    let currentButton = document.querySelector(buttonsId[index]);
+    currentButton.addEventListener('click', function(event){
+      let imgContainer = document.querySelector('#meme-image-container');
+      if(index == 0){
+        imgContainer.style.border = "3px dashed red";
+      }else if(index == 1){
+        imgContainer.style.border = "5px double blue";
+      }else if(index == 2){
+        imgContainer.style.border = "6px groove green";
+      }
+    })
+  }
+}
+
+function addListenerToMemeTemplate(){
+  let memeTemplateContainer = document.querySelectorAll('.meme-template')
+
+  for(let index = 0; index < memeTemplateContainer.length; index += 1){
+    memeTemplateContainer[index].addEventListener('click', function(event){
+      let img = document.querySelector('#meme-image');
+      let memeTemplate = event.target.src;
+      img.src = memeTemplate;
+    })
+  }
+
 }
