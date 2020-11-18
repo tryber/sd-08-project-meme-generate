6f38bdce283ca4a09d77ee6dba5b059bfa762b8a
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-18 01:22:32 -0300

    Codeclimate

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
+const img = document.querySelector('#meme-image');
+
 function addListenerToTextToMeme() {
-  let textInput = document.querySelector('#text-input');
-  let memeText = document.querySelector('#meme-text')
+  const textInput = document.querySelector('#text-input');
+  const memeText = document.querySelector('#meme-text')
   textInput.addEventListener('keyup', function (event) {
     memeText.innerHTML = event.target.value;
   })
 }
 
 function handleFiles(event) {
-  let img = document.querySelector('#meme-image');
   img.setAttribute('src', '');
   img.src = URL.createObjectURL(event.target.files[0]);
   img.onload = function () {
     URL.revokeObjectURL(img.src) // free memory
   }
-  /* now you can work with the file list */
+  /* now you can work with the file list https://developer.mozilla.org/pt-BR/docs/Web/API/File/Using_files_from_web_applications */
 }
 
 function addListenerToBorderButtons() {
-  let buttonsId = ['#fire', '#water', '#earth'];
+  const buttonsId = ['#fire', '#water', '#earth'];
   for (let index = 0; index < buttonsId.length; index += 1) {
-    let currentButton = document.querySelector(buttonsId[index]);
+    const currentButton = document.querySelector(buttonsId[index]);
     currentButton.addEventListener('click', function (event) {
-      let imgContainer = document.querySelector('#meme-image-container');
+      const imgContainer = document.querySelector('#meme-image-container');
       if (index == 0) {
         imgContainer.style.border = "3px dashed red";
       } else if (index == 1) {
@@ -34,18 +35,17 @@ function addListenerToBorderButtons() {
 }
 
 function addListenerToMemeTemplate() {
-  let memeTemplateContainer = document.querySelectorAll('.meme-template')
+  const memeTemplateContainer = document.querySelectorAll('.meme-template')
   for (let index = 0; index < memeTemplateContainer.length; index += 1) {
     memeTemplateContainer[index].addEventListener('click', function (event) {
-      let img = document.querySelector('#meme-image');
-      let memeTemplate = event.target.src;
+      const memeTemplate = event.target.src;
       img.src = memeTemplate;
     })
   }
 }
 
-window.onload = function() {
+window.onload = function () {
   addListenerToTextToMeme();
   addListenerToBorderButtons();
   addListenerToMemeTemplate();
-}
+};
