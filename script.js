ce5f682d64bfbed1a355f868c3875544fffc5b64
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-11-22 14:15:14 -0300

    Upload de imagem

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const memeInsert = document.querySelector('input[name=image]');
 const textInput = document.getElementById('text-input');
 const text = document.getElementById('meme-text');
+const memeImage = document.getElementById('meme-image');
 
 function inputChanged() {
   textInput.addEventListener('keyup', function () {
@@ -7,3 +9,18 @@ function inputChanged() {
   });
 }
 inputChanged();
+
+function imagePreview() {
+  memeInsert.addEventListener('change', function () {
+    const reader = new FileReader();
+    reader.onloadend = function () {
+      memeImage.src = reader.result;
+    };
+    if (memeInsert) {
+      reader.readAsDataURL(memeInsert.files[0]);
+    } else {
+      memeImage.src = '';
+    }
+  });
+}
+imagePreview();
