4328d9663892b651ecfe85b3bb29a4e5983adec1
Doug-Fernando <doug.fernando95@gmail.com>
2020-12-19 20:45:41 -0300

    Req 2

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const memeImage = document.querySelector('#meme-image');
+const memeImage = document.querySelector('#meme-insert');
 const textInput = document.querySelector('#text-input');
 memeImage.addEventListener('change', () => {
-  document.querySelector('#blah').src = URL.createObjectURL(memeImage.files[0]);
+  document.querySelector('#meme-image').src = URL.createObjectURL(
+    memeImage.files[0]
+  );
 });
 
 textInput.addEventListener('keyup', () => {
