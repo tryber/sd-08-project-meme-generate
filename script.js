1fdd4b483fcb2c7c138d9de129fc3d5e210bfb08
emassaki <massakihirayama@gmail.com>
2020-12-21 10:46:55 -0300

    Refatorado css e html para melhor visibilidade

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Adiciona o texto no meme
 const textInput = document.getElementById('text-input');
 textInput.addEventListener('keyup', () => {
   const upperText = document.getElementById('meme-text');
   upperText.innerHTML = textInput.value;
 });
 
-// Adiciona a imagem ao container
 const imageInput = document.getElementById('meme-insert');
 imageInput.addEventListener('change', (event) => {
   const memeImage = document.getElementById('meme-image');
@@ -15,33 +13,33 @@ imageInput.addEventListener('change', (event) => {
   };
 });
 
-// Botões de estilo
 const buttonsContainer = document.querySelector('.buttons');
 const memeContainer = document.getElementById('meme-image-container');
 buttonsContainer.addEventListener('click', (event) => {
-  if (event.target.id === 'fire') {
-    memeContainer.style.border = '3px dashed red';
-  }
-  if (event.target.id === 'water') {
-    memeContainer.style.border = '5px double blue';
-  }
-  if (event.target.id === 'earth') {
-    memeContainer.style.border = '6px groove green';
+  switch (event.target.id) {
+    case 'fire':
+      memeContainer.style.border = '3px dashed red';
+      break;
+    case 'water':
+      memeContainer.style.border = '5px double blue';
+      break;
+    case 'earth':
+      memeContainer.style.border = '6px groove green';
+      break;
   }
 });
 
-// Exemplos de meme
 const samples = document.querySelector('.meme-samples');
 for (let memeNumber = 1; memeNumber <= 4; memeNumber += 1) {
   const images = document.createElement('img');
   images.id = `meme-${memeNumber}`;
   images.src = `/imgs/meme${memeNumber}.png`;
-  images.width = 100;
-  images.height = 100;
+  // images.width = 100;
+  // images.height = 100;
   images.style.border = '1px solid black';
   samples.appendChild(images);
 }
-// Adiciona o exemplo no container
+
 samples.addEventListener('click', (event) => {
   for (let memeNumber = 1; memeNumber <= 4; memeNumber += 1) {
     if (event.target.id === `meme-${memeNumber}`) {
