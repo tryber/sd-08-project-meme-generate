98bc380ea433cf6c1707b21f37a66687a74396c6
Thiago Ferreira <thiagocristhian@gmail.com>
2020-12-30 17:49:40 -0300

    Requisito 6 - corrigindo codeclimate

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ const button1 = document.getElementById('fire');
 const image = document.getElementById('meme-image-container');
 button1.addEventListener('click', () => {
   image.style.border = '3px dashed red';
-})
+});
 
 const button2 = document.getElementById('water');
 button2.addEventListener('click', () => {
   image.style.border = '5px double blue';
-})
+});
 
 const button3 = document.getElementById('earth');
 button3.addEventListener('click', () => {
   image.style.border = '6px groove green';
-})
+});
