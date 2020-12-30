ff50c34b6c65d501affb41c99a2cf4b4c8933bd5
Thiago Ferreira <thiagocristhian@gmail.com>
2020-12-30 16:22:13 -0300

    Requisitos 1 - 5 - corrigindo codeclimate

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const memeImage = document.querySelector('#meme-insert');
 const textInput = document.querySelector('#text-input');
-const borders = document.querySelectorAll('.border');
-const memes = document.querySelectorAll('#memes');
+
 memeImage.addEventListener('change', () => {
   const selectImage = memeImage.files[0];
   document.querySelector('#meme-image').src = URL.createObjectURL(selectImage);
