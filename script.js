190aaf540910a351a5c551943c6d0e1df78f3c48
Thiago Ferreira <thiagocristhian@gmail.com>
2020-12-30 17:43:41 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-const memeImage = document.querySelector('#meme-insert');
-const textInput = document.querySelector('#text-input');
+const memeImage = document.getElementById('meme-insert');
+const textInput = document.getElementById('text-input');
 
 memeImage.addEventListener('change', () => {
   const selectImage = memeImage.files[0];
-  document.querySelector('#meme-image').src = URL.createObjectURL(selectImage);
+  document.getElementById('meme-image').src = URL.createObjectURL(selectImage);
 });
 
 textInput.addEventListener('keyup', () => {
-  const memeText = document.querySelector('#meme-text');
+  const memeText = document.getElementById('meme-text');
   memeText.innerHTML = textInput.value;
 });
+
+const button1 = document.getElementById('fire');
+const image = document.getElementById('meme-image-container');
+button1.addEventListener('click', () => {
+  image.style.border = '3px dashed red';
+})
+
+const button2 = document.getElementById('water');
+button2.addEventListener('click', () => {
+  image.style.border = '5px double blue';
+})
+
+const button3 = document.getElementById('earth');
+button3.addEventListener('click', () => {
+  image.style.border = '6px groove green';
+})
